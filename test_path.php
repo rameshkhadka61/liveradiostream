aa86e23dfd314a70b7d423c3b4c4a742ef361afe
Ramesh Khadka <rameshkhadka61@gmail.com>
2026-06-21 20:34:27 +0545

    Remove test files

--- test_path.php
+++
@@ -1,8 +0,0 @@
-<?php
-$path = 'D:\xampp\htdocs\scraper\images\Radio_Tahalka_91_9.jpg';
-echo "Original: " . $path . "\n";
-echo "Exists: " . (file_exists($path) ? 'Yes' : 'No') . "\n";
-
-$forward_path = str_replace('\\', '/', $path);
-echo "Forward: " . $forward_path . "\n";
-echo "Exists: " . (file_exists($forward_path) ? 'Yes' : 'No') . "\n";
