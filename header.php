614812ded46e94e71696f0a971ae7b9d69de2335
Ramesh Khadka <rameshkhadka61@gmail.com>
2026-06-24 16:25:06 +0545

    Remove hardcoded meta description from header for SEO compatibility; bump version to 1.9.38

--- header.php
+++ header.php
@@ -9,7 +9,6 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="<?php bloginfo( 'description' ); ?>">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="google-adsense-anchor-ad-position" content="top">
     <?php wp_head(); ?>
